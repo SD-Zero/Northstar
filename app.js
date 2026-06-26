20cf154c87db2d0ca4cd31ccd8ffb367652f67b3
SD-Zero <mdnvju.2021@gmail.com>
2026-06-26 16:19:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-const layers = document.getElementById("layers");
-const button = document.getElementById("collapseBtn");
 
-button.onclick = () => {
-
-    layers.classList.toggle("collapsed");
-
-    if(layers.classList.contains("collapsed")){
-
-        button.textContent = "❮";
-
-    }else{
-
-        button.textContent = "❯";
-
-    }
-
-}
